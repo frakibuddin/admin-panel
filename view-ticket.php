9dd41f0168951f077f5915d63181f40dcdeb08bf
frakibuddin <frakibuddin360@gmail.com>
2023-08-17 01:19:36 +0600

    update view page

--- view-ticket.php
+++ view-ticket.php
@@ -35,39 +35,67 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col">
-                            <div class="card mycard">
-                                <div class="mycard-heading">
-                                    <h2>Ticket</h2>
+                        <div class="col-lg-4 col-md-12">
+                            <div class="mycard p-5  text-center">
+                                <img src="assets/img/img_avatar2.png" class="mb-2 w-25 border border-3 rounded-circle border-color" alt="">
+                                <p class="text-color-black fw-bold mt-2">Name : Smit jon</p>
+                                <p class="text-color-black mt-2">Email : test@gmailcom</p>
+                                <p class="text-color-black mt-2">Phone : 98887973</p>
+
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-md-12">
+                            <div class="mycard msg-box p-5">
+                                <div class="msg-inner d-flex align-items-start ">
+                                    <div class="profile-img">
+                                        <img src="assets/img/img_avatar2.png" class="rounded-circle border-color" width="40" alt="">
+                                    </div>
+                                    <div class="msg mt-4">
+                                        <p class="text-color-black"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat amet quidem enim voluptatibus. Quasi dicta eveniet vero voluptates, laborum est sapiente minima soluta iure fugiat dolore, laudantium debitis harum.</p>
+                                        <div class="d-flex align-items-center justify-content-between pt-2">
+                                            <p class="fw-bolder text-muted">Mr jams</p>
+                                            <p class="text-muted">2/3/2023 - 12:06</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="my-card-body table-responsive">
-                                    <table id="model-list" class="model-list table table-border" style="width: 100%;">
-                                        <thead>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Test Ticket </th>
-                                            </tr>
-                                            <tr>
-                                                <th>Nubmer</th>
-                                                <th>84758 </th>
-                                            </tr>
-                                            <tr>
-                                                <th>Date</th>
-                                                <th>2/2/2023 </th>
-                                            </tr>
-                                        </thead>
-                                    </table>
+                                <div class="msg-inner d-flex align-items-start">
+                                    <div class="profile-img">
+                                        <img src="assets/img/img_avatar2.png" class="rounded-circle border-color" width="40" alt="">
+                                    </div>
+                                    <div class="msg mt-4">
+                                        <p class="text-color-black"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat amet quidem enim voluptatibus. Quasi dicta eveniet vero voluptates, laborum est sapiente minima soluta iure fugiat dolore, laudantium debitis harum.</p>
+                                        <div class="d-flex align-items-center justify-content-between pt-2">
+                                            <p class="fw-bolder text-muted">Mr jams</p>
+                                            <p class="text-muted">2/3/2023 - 12:06</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="msg-inner d-flex align-items-start">
+                                    <div class="profile-img">
+                                        <img src="assets/img/img_avatar2.png" class="rounded-circle border-color" width="40" alt="">
+                                    </div>
+                                    <div class="msg mt-4">
+                                        <p class="text-color-black"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat amet quidem enim voluptatibus. Quasi dicta eveniet vero voluptates, laborum est sapiente minima soluta iure fugiat dolore, laudantium debitis harum.</p>
+                                        <div class="d-flex align-items-center justify-content-between pt-2">
+                                            <p class="fw-bolder text-muted">Mr jams</p>
+                                            <p class="text-muted">2/3/2023 - 12:06</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="msg-inner d-flex align-items-start">
+                                    <div class="profile-img">
+                                        <img src="assets/img/img_avatar2.png" class="rounded-circle border-color" width="40" alt="">
+                                    </div>
+                                    <div class="msg mt-4">
+                                        <p class="text-color-black"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam placeat amet quidem enim voluptatibus. Quasi dicta eveniet vero voluptates, laborum est sapiente minima soluta iure fugiat dolore, laudantium debitis harum.</p>
+                                        <div class="d-flex align-items-center justify-content-between pt-2">
+                                            <p class="fw-bolder text-muted">Mr jams</p>
+                                            <p class="text-muted">2/3/2023 - 12:06</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="card mycard">
-                                <div class="mycard-heading">
-                                    <h2>Reply</h2>
-                                </div>
+                            <div class="mycard ">
                                 <div class="my-card-body">
                                     <form action="">
                                         <!-- error -->
@@ -78,28 +106,39 @@
 
                                         <div class="row">
                                             <div class="col-md-12">
-                                                <label for="" class="input-lavel">Brand</label>
+                                                <label for="" class="input-lavel">Ticket</label>
                                                 <div class="input-group mb-3">
-                                                    <span class="input-group-text" id="basic-addon1"><i class="fa-regular fa-bookmark"></i></span>
+                                                    <span class="input-group-text" id="basic-addon1"><i class="fa-regular fa-bookmark"></i> </span>
                                                     <input type="text" class="form-control" placeholder="test brand">
                                                 </div>
                                             </div>
                                             <div class="col-md-12 mt-2">
-                                                <label for="" class="input-lavel">Brand Slug</label>
+                                                <label for="" class="input-lavel">Number</label>
                                                 <div class="input-group mb-3">
                                                     <span class="input-group-text" id="basic-addon1"><i class="fa-regular fa-bookmark"></i></span>
                                                     <input type="text" class="form-control" placeholder="test brandryslug">
                                                 </div>
                                             </div>
                                             <div class="col-md-12 mt-2">
+                                                <label for="" class="input-lavel">Select</label>
+                                                <div class="input-group mb-3">
+                                                    <span class="input-group-text" id="basic-addon1"><i class="fa-regular fa-bookmark"></i></span>
+                                                    <select name="" class="form-control">
+                                                        <option value="">Select</option>
+                                                        <option value="" class="text-dark">option 1</option>
+                                                        <option value="" class="text-dark">option 2</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12 mt-2">
                                                 <label for="" class="input-lavel">Message</label>
                                                 <div class="input-group mb-3">
                                                     <span class="input-group-text" id="basic-addon1"><i class="fa-regular fa-bookmark"></i></span>
-                                                    <textarea class="form-control" rows="5" placeholder="Message write here"></textarea>
+                                                    <textarea class="form-control" placeholder="Write here" rows="6"></textarea>
                                                 </div>
                                             </div>
                                             <div class="col-md-12 mt-3">
-                                                <button class="submit-btn">Submit</button>
+                                                <button class="submit-btn">Send</button>
                                             </div>
                                         </div>
                                     </form>
